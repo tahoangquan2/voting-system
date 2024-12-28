74d08d234488ee61751fbda287741b4f707cfc65
PkKeo <minhcvn2011@gmail.com>
2024-12-28 10:25:28 +0700

    vote by level

--- voting/align_and_vote_word_level.py
+++ voting/align_and_vote_word_level.py
@@ -1,9 +1,111 @@
 import csv
-from collections import Counter
+from collections import Counter, defaultdict
 import pandas as pd
 import re
 
 import unicodedata
+
+
+# Expanded mapping of accents to their corresponding numbers
+ACCENT_TO_NUMBER = {
+    "á": 1, "à": 2, "ã": 3, "ả": 4, "ạ": 5,
+    "é": 1, "è": 2, "ẽ": 3, "ẻ": 4, "ẹ": 5,
+    "í": 1, "ì": 2, "ĩ": 3, "ỉ": 4, "ị": 5,
+    "ó": 1, "ò": 2, "õ": 3, "ỏ": 4, "ọ": 5,
+    "ú": 1, "ù": 2, "ũ": 3, "ủ": 4, "ụ": 5,
+    "ý": 1, "ỳ": 2, "ỹ": 3, "ỷ": 4, "ỵ": 5,
+    "ấ": 1, "ầ": 2, "ẫ": 3, "ẩ": 4, "ậ": 5,
+    "ế": 1, "ề": 2, "ễ": 3, "ể": 4, "ệ": 5,
+    "ố": 1, "ồ": 2, "ỗ": 3, "ổ": 4, "ộ": 5,
+    "ắ": 1, "ằ": 2, "ẵ": 3, "ẳ": 4, "ặ": 5,
+    "ớ": 1, "ờ": 2, "ỡ": 3, "ở": 4, "ợ": 5,
+    "ứ": 1, "ừ": 2, "ữ": 3, "ử": 4, "ự": 5#,
+    #"đ": 9  # For 'đ', assigning a unique number
+}
+
+# Mapping of accented characters to their base (unmarked) characters
+ACCENT_TO_BASE = {
+    "á": "a", "à": "a", "ã": "a", "ả": "a", "ạ": "a",
+    "é": "e", "è": "e", "ẽ": "e", "ẻ": "e", "ẹ": "e",
+    "í": "i", "ì": "i", "ĩ": "i", "ỉ": "i", "ị": "i",
+    "ó": "o", "ò": "o", "õ": "o", "ỏ": "o", "ọ": "o",
+    "ú": "u", "ù": "u", "ũ": "u", "ủ": "u", "ụ": "u",
+    "ý": "y", "ỳ": "y", "ỹ": "y", "ỷ": "y", "ỵ": "y",
+    "â": "â", "ấ": "â", "ầ": "â", "ẫ": "â", "ẩ": "â", "ậ": "â",
+    "ê": "ê", "ế": "ê", "ề": "ê", "ễ": "ê", "ể": "ê", "ệ": "ê",
+    "ô": "o", "ố": "o", "ồ": "o", "ỗ": "o", "ổ": "o", "ộ": "o",
+    "ă": "ă", "ắ": "ă", "ằ": "ă", "ẵ": "ă", "ẳ": "ă", "ặ": "ă",
+    "ơ": "ơ", "ớ": "ơ", "ờ": "ơ", "ỡ": "ơ", "ở": "ơ", "ợ": "ơ",
+    "ư": "ư", "ứ": "ư", "ừ": "ư", "ữ": "ư", "ử": "ư", "ự": "ư"
+}
+
+
+
+def normalize_vietnamese(word):
+    normalized_word = ""
+    accent_number = ""
+    word = word.lower()
+    for char in word:
+        if char in ACCENT_TO_NUMBER:
+            normalized_word += ACCENT_TO_BASE[char]  # Replace with the base character
+            accent_number = str(ACCENT_TO_NUMBER[char])  # Set the corresponding accent number
+        else:
+            normalized_word += char  # Add the character as is if no accent
+
+    return normalized_word + accent_number
+
+
+
+
+import difflib
+
+def load_words(file_path):
+    """Load words from the given text file."""
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return [line.strip() for line in file]
+
+def load_normalize_words(file_path):
+    """Load words from the given text file."""
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return [normalize_vietnamese(line.strip()) for line in file]
+
+def find_similar_words(word, word_list, max_results=5):
+    """
+    Find similar words using difflib.
+    - Returns up to `max_results` similar words from the word list.
+    """
+    similar_list  = []
+    normalize_word = normalize_vietnamese(word)
+    cnt = 0
+    for check in word_list:
+        normalize_check = normalize_vietnamese(check)
+        similar, dis = words_similar(normalize_word, normalize_check, threshold= 0.15)
+
+        if similar:
+            similar_list.append(check)
+            cnt += 1
+
+        if(cnt >= max_results):
+            break
+    return similar_list
+
+
+
+def check_word_in_file(word, vietnam_word_list, normalize_vietnam_list):
+    """
+    Check if a word is in the file and return suggestions if it's not found.
+    - If found, return "Yes".
+    - If not, return "No" and a list of similar words.
+    """
+
+    normalize_word = normalize_vietnamese(word)
+    normalize_word = re.sub(r'[.,:_;?/"()…]', '', normalize_word)
+    if normalize_word in normalize_vietnam_list:
+        similar_words = [word]
+        return True, similar_words
+    else:
+        similar_words = find_similar_words(word, vietnam_word_list)
+        return False, similar_words
 
 
 def normalize_text(text):
@@ -16,6 +118,10 @@ def normalize_text(text):
     # Convert "«»" and to '""'
     text = text.replace("« ", '"')
     text = text.replace(" »", '"')
+
+    text = text.replace("_", '')
+    text = text.replace("~", '')
+    text = text.replace("—", '-')
 
     # Convert '...' to '…'
     text = re.sub(r'\.\.\.', '…', text)
@@ -53,6 +159,7 @@ def normalize_word(word):
         return word
     # Convert to lowercase
     word = word.lower()
+
     # Remove accents by decomposing the Unicode characters and filtering
     word = ''.join(
         char for char in unicodedata.normalize('NFD', word)
@@ -61,9 +168,7 @@ def normalize_word(word):
     word = re.sub(r'-', '', word)
     word = re.sub(r'f', 't', word)
     word = re.sub(r'j', 'i', word)
-    word = re.sub(r'1', 'i', word)
-    word = re.sub(r'4', 'a', word)
-    word = re.sub(r'7', '?', word)
+
 
     return word
 
@@ -193,7 +298,8 @@ def MED_to_word(sen1, sen2):
     return aligned_seq1, aligned_seq2
 
 
-def align_multiple_sequences(sequences):
+def align_multiple_sequences(sequences, best_ocr = 1):
+    cnt = len(sequences)
     try_case = 0
     while try_case < len(sequences):
         aligned_sequences = sequences[:]
@@ -216,18 +322,18 @@ def align_multiple_sequences(sequences):
                 aligned_sequences.append(base_sequence)
                 return aligned_sequences, 0
 
-            # print(f"try_case {try_case} - limit {limit}:")
-            # for seq in aligned_sequences:
-            #     print(seq)
+
 
             limit += 1
 
         try_case += 1
-        # print(f"Try case {try_case} done")
+
 
     # If can't align return the best ocr result
-    best_ocr = 1
-    return [sequences[best_ocr], sequences[best_ocr], sequences[best_ocr], sequences[best_ocr]], 1
+    res = []
+    for i in range(cnt):
+        res.append(sequences[best_ocr])
+    return res, 1
 
 
 def read_ocr_inputs(csv_file):
@@ -241,117 +347,190 @@ def read_ocr_inputs(csv_file):
     return ocr_outputs
 
 
-def vote_characters(aligned_outputs):
-    # Perform voting for each character position
-    voted_result = []
-    for i in range(len(aligned_outputs[0])):
-        # Gather characters at the current position
-        chars_at_pos = [output[i] for output in aligned_outputs]
+import string
 
-        # Count character frequencies
-        char_counts = Counter(chars_at_pos)
+def most_common_end_punctuation(strings):
+    """
+    Find the most common ending punctuation in a list of strings.
+    :param strings: List of strings
+    :return: The most common ending punctuation and its count
+    """
+    end_punctuations = []
 
-        # Extract the most frequent characters
-        most_common = char_counts.most_common()
+    # Define punctuation characters
+    punctuation_set = {'?', '!', '.', ';' , ',', ']', '}', '%' , '$', '…'}
 
-        # Apply tiebreaker rules:
-        if len(most_common) > 1 and most_common[0][1] == most_common[1][1]:  # A tie exists
-            char1, count1 = most_common[0]
-            char2, count2 = most_common[1]
+    # Collect ending punctuation from each string
+    for s in strings:
+        if s and s[-1] in punctuation_set:
+            end_punctuations.append(s[-1])
 
-            if char1 == '`' and char2 == ' ':
-                voted_result.append('`')  # Backquote beats space
-            elif char2 == '`' and char1 == ' ':
-                voted_result.append('`')  # Backquote beats space
-            elif char1 == '`':
-                voted_result.append(char2)  # Other character beats backquote
-            elif char2 == '`':
-                voted_result.append(char1)  # Other character beats backquote
-            else:
-                voted_result.append(char1)  # Default to the first one if no special cases
+    # Use Counter to find the most common ending punctuation
+    if end_punctuations:
+        counter = Counter(end_punctuations)
+        return counter.most_common(1)[0][0]  # Return the most common punctuation and its count
+    else:
+        return '' # No ending punctuation found
+
+def most_common_start_punctuation(strings):
+    """
+    Find the most common start punctuation in a list of strings.
+    :param strings: List of strings
+    :return: The most common start punctuation and its count
+    """
+    start_punctuations = []
+
+    # Define punctuation characters
+    punctuation_set = ['-', '"', '(' , '{', '[']
+
+    # Collect ending punctuation from each string
+    for s in strings:
+        if s and s[0] in punctuation_set:
+            start_punctuations.append(s[0])
+
+    # Use Counter to find the most common ending punctuation
+    if start_punctuations:
+        counter = Counter(start_punctuations)
+        return counter.most_common(1)[0][0]  # Return the most common punctuation and its count
+    else:
+        return '' # No ending punctuation found
+
+def is_integer(s):
+    """
+    Check if a string represents an integer.
+    :param s: Input string
+    :return: True if the string represents an integer, False otherwise
+    """
+    try:
+        int(s)  # Try converting the string to an integer
+        return True
+    except ValueError:
+        return False
+
+def vote(list_word, vietnam_word_list, normalize_vietnam_list):
+
+    vote_container = []
+    weight_container = []
+    begin_punctuation = most_common_start_punctuation(list_word)
+    end_punctuation = most_common_end_punctuation(list_word)
+    for i in range(len(list_word)):
+        nw = re.sub(r'[.,:_;?/"()…]', '', list_word[i])
+
+        in_list, similar_words = check_word_in_file(nw, vietnam_word_list, normalize_vietnam_list)
+        if in_list:
+            for j in range(len(list_word)):
+                if(i != j):
+                    now = re.sub(r'[.,:_;?/"()…]', '', list_word[j])
+                    if(now == nw):
+                        return list_word[i], 2004
+
+    for word in list_word:
+        org_word = word
+        word = re.sub(r'[.,:_;?/"()…]', '', word)
+        if word == 'None':
+            continue
+        if is_integer(word):
+            vote_container.append(word)
+            weight_container.append(1)
+            continue
+        in_list, similar_words = check_word_in_file(word, vietnam_word_list, normalize_vietnam_list)
+        if in_list:
+            for other_word in list_word:
+                if org_word != other_word:  # Avoid comparing the same element
+                    other_word = re.sub(r'[.,:_;?/"(){}[]…]', '', other_word)
+                    nw = normalize_vietnamese(normalize_word(word))
+                    now = normalize_vietnamese(normalize_word(other_word))
+                    similar, dist = words_similar(nw, now, threshold = 0.15)
+                    if(similar):
+                            return begin_punctuation + word + end_punctuation, 2011
+
+            vote_container.append(word)
+            weight_container.append(1)
+            continue
+        for sim in similar_words:
+            vote_container.append(sim)
+            dist = levenshtein_distance(sim, word)
+            weight_container.append(0.54 * (1 - dist/max(len(word), len(sim))))
+
+    if not vote_container:
+        return None, 0  # Return None if the list is empty
+
+    if len(vote_container) != len(weight_container):
+        raise ValueError("vote_container and weight_container must have the same length.")
+
+        # Aggregate weights for each unique vote
+    weight_sum = defaultdict(float)
+    for vote, weight in zip(vote_container, weight_container):
+        weight_sum[vote] += weight
+
+    # Find the item with the highest total weight
+    most_common = max(weight_sum.items(), key=lambda item: item[1])
+    most_common_item, total_weight = most_common
+
+    return begin_punctuation + most_common_item + end_punctuation , total_weight
+
+
+from openpyxl import load_workbook
+
+def read_and_vote(file_path, vietnam_word_list, normalize_vietnam_list ):
+    # Load the workbook and select the active sheet
+    workbook = load_workbook(file_path)
+    sheet = workbook.active
+
+    # Iterate through rows starting from the second row
+    rowID = 1
+    for row in sheet.iter_rows(min_row=2,max_row = 360, min_col=2, max_col=4):  # Columns B, C, D
+        print(f"Process on row {rowID}")
+
+        sentence1 = normalize_text(str(row[0].value))  # Column B
+        sentence2 = normalize_text(str(row[1].value))   # Column C
+        sentence3 = normalize_text(str(row[2].value))  # Column D
+        sequences = [sentence1, sentence2, sentence3]
+        align_sentences, success = align_multiple_sequences(sequences)
+        vote_sentence_splits = []
+        if (success == '1'):
+            vote_sentence = align_sentences[0]
         else:
-            # No tie or clear winner, choose the most common character
-            voted_result.append(most_common[0][0])
+            sentences_splits = []
+            for align in align_sentences:
+                list = align.split(' ')
+                sentences_splits.append(list)
+            for i in range(len(sentences_splits[0])):
+                word1 = sentences_splits[0][i]
+                word2 = sentences_splits[1][i]
+                word3 = sentences_splits[2][i]
 
-    # Return the final corrected string
-    return ''.join(voted_result)
+                list_word_to_vote = [word1, word2, word3]
+
+                vote_word = vote(list_word_to_vote,vietnam_word_list, normalize_vietnam_list)
+
+                if(vote_word[0]):
+                    vote_sentence_splits.append(vote_word[0])
+
+            vote_sentence = ' '.join(vote_sentence_splits)
 
 
-def refine_output(output):
-    output = output.replace('`', '')
-    return output
+        # Write the processed value to column E (fifth column)
+        sheet.cell(row=row[0].row, column=5, value=vote_sentence)
+        rowID +=1
+
+    # Save changes to the workbook
+    workbook.save(file_path)
+    print(f"Processed workbook saved: {file_path}")
 
 
-def add_voted_results(csv_file, voted_results):
-    file_path = csv_file
-    df = pd.read_csv(file_path, header=None, encoding='utf-8')
 
-    # Step 3: Add the new column
-    df[len(df.columns)] = voted_results  # Adds the list as a new column
-
-    # Step 4: Save the updated DataFrame back to a CSV file
-    df.to_csv('output.csv', index=False, header=False, encoding='utf-8')
-    df.to_excel('output.xlsx', index=False, header=False)
-
-    print(f"New column added")
 
 
 if __name__ == "__main__":
-    # # Debug for specific row
-    # ocr_outputs = read_ocr_inputs('input.csv')[260]
-    # norm_ocr_outputs = []
-    # for each in ocr_outputs:
-    #     each = normalize_text(each)
-    #     norm_ocr_outputs.append(each)
-    # # print(norm_ocr_outputs)
-
-    # aligned_result = align_multiple_sequences(norm_ocr_outputs)
-    # for seq in aligned_result:
-    #     print(f"'{seq}',")
-
-    # final_ouputs = []
-    # failed_list = []
-    # ocr_outputs = read_ocr_inputs('input.csv')
-    # for index, line in enumerate(ocr_outputs):
-    #     print(f"[!] Processing row {index+1}")
-    #
-    #     norm_ocr_outputs = []
-    #     for each in line:
-    #         norm_ocr_outputs.append(normalize_text(each))
-    #
-    #     aligned_result, fail = align_multiple_sequences(norm_ocr_outputs)
-    #     if fail == 1:
-    #         failed_list.append(index + 1)
-    #     for seq in aligned_result:
-    #         print(f'"{seq}",')
-    #
-    #     voted_output = vote_characters(aligned_result)
-    #     final_ouputs.append(refine_output(voted_output))
-    #
-    # print(f"Number of failed rows: {len(failed_list)}")
-    # print(f"Failed rows: {failed_list}")
-    # add_voted_results('input.csv', final_ouputs)
-
-    sentence1 = "dtrong xám, trên những manh ruộng và những đám đất"
-    sentence2 = "dưrùng Xán, Irèn những manb FuQ g 3à nhữrng dám dất"
-    sentence3 = "đường xá:n, trên những rCmănh ruộng và những đám đất"
-
-    sentence1 = normalize_text(sentence1)
-    sentence2 = normalize_text(sentence2)
-    sentence3 = normalize_text(sentence3)
-
-    # al1, al2 = MED_to_word(sentence3, sentence1)
-    # print(al1)
-    # print(al2)
-
-    sequences = []
-    sequences.append(sentence1)
-    sequences.append(sentence2)
-    sequences.append(sentence3)
-    align = align_multiple_sequences(sequences)
-
-    print(align)
-
+    # Example usage
+    file_path = "ocr_results.xlsx"  # Replace with your file path
+    vietnamese_file_path = 'VN_MorphoSyllable_List.txt'
+    vietnam_word_list = load_words(vietnamese_file_path)
+    normalize_vietnam_list = load_normalize_words(vietnamese_file_path)
+    read_and_vote(file_path, vietnam_word_list, normalize_vietnam_list)
+    # list_word = ['PHAN', 'PHẦN', 'PHẦN']
+    # print(vote(list_word, vietnam_word_list, normalize_vietnam_list))
 
 
 
